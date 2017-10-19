bd6497781520f4fa59761748cadbdadcc76a270a
audishos <audisho.sada@gmail.com>
2017-10-19 16:19:18 -0400

    Implements bet factory and unit tests for validations

--- even-better-server/app/models/bet.rb
+++ even-better-server/app/models/bet.rb
@@ -3,4 +3,10 @@ class Bet < ApplicationRecord
   has_many :messages
   belongs_to :mediator, class_name: :user
   belongs_to :creator, class_name: :user
+
+  validates :title, presence: true
+  validates :betting_deadline, presence: true
+  validates :outcome_deadline, presence: true
+  validates :mediator, presence: true
+  validates :creator, presence: true
 end
---
+++ even-better-server/spec/models/bet_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe Bet, type: :model do
+
+  context 'validations' do
+
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:betting_deadline) }
+    it { should validate_presence_of(:outcome_deadline) }
+    it { should validate_presence_of(:mediator) }
+    it { should validate_presence_of(:creator) }
+
+    it 'should create a Bet object' do
+      bet = FactoryGirl.build(:bet)
+      expect(bet).to be_a(Bet)
+    end
+
+  end
+
+end

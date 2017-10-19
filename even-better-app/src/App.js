8c0c96ff9107ea610cb8efbd123b6da7b2fee227
Rahul Ramesh <rahul.ramesh888@gmail.com>
2017-10-19 15:57:51 -0400

    Started coding the React App

--- even-better-app/src/App.js
+++ even-better-app/src/App.js
@@ -1,22 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import UsersContainer from './components/UsersContainer'
 
 class App extends Component {
-  
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-header">
+          <h1>Idea Board</h1>
+        </div>
+        <UsersContainer />
       </div>
     );
   }
 }
 
-export default App;
+export default App

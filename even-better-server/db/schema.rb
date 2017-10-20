f270e0ba0415498bc9eca4f8062b8694112e3437
audishos <audisho.sada@gmail.com>
2017-10-20 13:04:57 -0400

    Adds migration to make email and username unique

--- even-better-server/db/schema.rb
+++ even-better-server/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171018194937) do
+ActiveRecord::Schema.define(version: 20171020170344) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,8 @@ ActiveRecord::Schema.define(version: 20171018194937) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "bets", "possibilities", column: "outcome_id"

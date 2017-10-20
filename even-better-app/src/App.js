3f2a691100cc9b739e40e6037fc55ea60ca8601e
Craig Miles-Wilde <craigmilde_@hotmail.com>
2017-10-20 17:05:27 -0400

    material-ui active with some navbar in app

--- even-better-app/src/App.js
+++ even-better-app/src/App.js
@@ -2,15 +2,34 @@ import React, { Component } from 'react'
 import './App.css'
 import UsersContainer from './components/UsersContainer'
 
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import AppBar from 'material-ui/AppBar';
+
+import injectTapEventPlugin from 'react-tap-event-plugin';
+ 
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h1>Testing the Even-Better backend API</h1>
+
+      <MuiThemeProvider>
+        <AppBar
+            title="Title"
+            iconClassNameRight="muidocs-icon-navigation-expand-more"
+          />
+          
+        <div className="App">
+          <div className="App-header">
+            <h1>Testing the Even-Better backend API</h1>
+          </div>
+          <UsersContainer />
         </div>
-        <UsersContainer />
-      </div>
+      </MuiThemeProvider>
     );
   }
 }

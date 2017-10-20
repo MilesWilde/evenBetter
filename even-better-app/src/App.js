b00def81445b191d0d9c0a56281c4610de584d50
Rahul Ramesh <rahul.ramesh888@gmail.com>
2017-10-20 17:23:14 -0400

    Installed material UI

--- even-better-app/src/App.js
+++ even-better-app/src/App.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react'
 import './App.css'
 import UsersContainer from './components/UsersContainer'
+import injectTapEventPlugin from 'react-tap-event-plugin'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import AppBar from 'material-ui/AppBar'
+
+injectTapEventPlugin();
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h1>Testing the Even-Better backend API</h1>
+      <MuiThemeProvider>
+        <AppBar
+            title="Title"
+            iconClassNameRight="muidocs-icon-navigation-expand-more"
+          />
+          
+       <div className="App">
+          <div className="App-header">
+            <h1>Testing the Even-Better backend API</h1>
+          </div>
+          <UsersContainer />
         </div>
-        <UsersContainer />
-      </div>
+      </MuiThemeProvider>
     );
   }
 }

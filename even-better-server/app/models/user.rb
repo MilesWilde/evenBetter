8f124bb5b85cf62cac5c87a9a6bdc5d169656c66
audishos <audisho.sada@gmail.com>
2017-10-20 11:52:08 -0400

    Implements https://scotch.io/tutorials/build-a-restful-json-api-with-rails-5-part-two up to Authenticate User

--- even-better-server/app/models/user.rb
+++ even-better-server/app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+
+  has_secure_password
+
   has_and_belongs_to_many :bets
   has_many :messages
 
--- even-better-server/spec/rails_helper.rb
+++ even-better-server/spec/rails_helper.rb
@@ -34,6 +34,8 @@ Shoulda::Matchers.configure do |config|
   end
 end
 
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
@@ -62,4 +64,8 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.include FactoryGirl::Syntax::Methods
+
+  config.include RequestSpecHelper
+  config.include ControllerSpecHelper
 end

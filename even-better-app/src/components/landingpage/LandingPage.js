1aecbb51ba069be3e5b60b3e0811535f918d1272
Rahul Ramesh <rahul.ramesh888@gmail.com>
2017-10-21 14:52:14 -0400

    Created text within the bets tabs for testing

--- even-better-app/src/components/landingpage/LandingPage.js
+++ even-better-app/src/components/landingpage/LandingPage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import './LandingPage.css';
+import './css/LandingPage.css';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
 import InviteColumn from './InviteColumn'
--- even-better-app/src/components/landingpage/TabsForBets.js
+++ even-better-app/src/components/landingpage/TabsForBets.js
@@ -1,16 +1,57 @@
 import React from 'react';
 import {Tabs, Tab} from 'material-ui/Tabs';
-import MapsPersonPin from 'material-ui/svg-icons/maps/person-pin';
 
-const TabsForBets = () => (
-  <Tabs>
-    <Tab
-      label="SPORTS BET"
-    />
-    <Tab
-      label="PERSONAL BET"
-    />
-  </Tabs>
-);
+const styles = {
+  headline: {
+    fontSize: 24,
+    paddingTop: 16,
+    marginBottom: 12,
+    fontWeight: 400,
+  },
+};
 
-export default TabsForBets;
+export default class TabsForBets extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 'a',
+    };
+  }
+
+  handleChange = (value) => {
+    this.setState({
+      value: value,
+    });
+  };
+
+  render() {
+    return (
+      <Tabs
+        value={this.state.value}
+        onChange={this.handleChange}
+      >
+        <Tab label="SPORTS BET" value="a">
+          <div>
+            <h2 style={styles.headline}>Place your sports bets here!</h2>
+            <p>
+              Tabs are also controllable if you want to programmatically pass them their values.
+              This allows for more functionality in Tabs such as not
+              having any Tab selected or assigning them different values.
+            </p>
+          </div>
+        </Tab>
+        <Tab label="PERSONAL BET" value="b">
+          <div>
+            <h2 style={styles.headline}>Bet on anything you want!</h2>
+            <p>
+              This is another example of a controllable tab. Remember, if you
+              use controllable Tabs, you need to give all of your tabs values or else
+              you wont be able to select them.
+            </p>
+          </div>
+        </Tab>
+      </Tabs>
+    );
+  }
+}

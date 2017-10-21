7204dbefe1fca8813114a08e86906fa221d1cf08
Rahul Ramesh <rahul.ramesh888@gmail.com>
2017-10-21 10:46:52 -0400

    Coded users and games endpoints for the API

--- even-better-server/config/routes.rb
+++ even-better-server/config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :users  
+      resources :users
+      resources :games 
+       
     end
   end
 end

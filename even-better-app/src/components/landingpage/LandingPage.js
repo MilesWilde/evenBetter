5c464ac1800a8555142309a910e9ccc15e92b7f8
Rahul Ramesh <rahul.ramesh888@gmail.com>
2017-10-21 14:34:19 -0400

    Coded dialog button on landing page

--- even-better-app/src/components/landingpage/LandingPage.js
+++ even-better-app/src/components/landingpage/LandingPage.js
@@ -6,6 +6,7 @@ import InviteColumn from './InviteColumn'
 import PointsColumn from './PointsColumn'
 import BetsColumn from './BetsColumn'
 import Buttons from './Buttons'
+import PopupBets from './PopupBets'
 
 
 class LandingPage extends Component {
@@ -16,6 +17,9 @@ class LandingPage extends Component {
         <MuiThemeProvider>
           <div className=" create-bet-buttons container">
             <Buttons />
+          </div>
+          <div className=" create-bet-buttons container">
+            <PopupBets />
           </div>
           <div className=" users-columns container">
             <div className="invite-column column-center"><InviteColumn /></div>

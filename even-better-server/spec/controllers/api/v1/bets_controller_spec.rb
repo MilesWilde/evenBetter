feaaf0311b414817297bd6d1fbab176b6a0a83b7
audishos <audisho.sada@gmail.com>
2017-10-23 15:33:13 -0400

    Fixes bet POST specs

--- even-better-server/spec/controllers/api/v1/bets_controller_spec.rb
+++ even-better-server/spec/controllers/api/v1/bets_controller_spec.rb
@@ -3,13 +3,13 @@ require 'rails_helper'
 RSpec.describe 'Bets API v1', type: :request do
   # initialize the test data
   let(:user) { create(:user) }
+  let(:users) { create_list(:user, 10) }
   let!(:bets) { create_list(:bet, 10, creator: user, users: [user]) }
-  let(:id) { bets.first.id }
+  let(:bet_id) { bets.first.id }
   let(:headers) { valid_headers }
 
   # test suite for GET /api/v1/bets
   describe 'GET /api/v1/bets' do
-
     before { get '/api/v1/bets', headers: headers}
 
     it 'returns bets' do
@@ -22,6 +22,33 @@ RSpec.describe 'Bets API v1', type: :request do
     end
   end
 
+  describe 'GET /api/v1/bets/:id' do
+    before { get "/api/v1/bets/#{bet_id}", headers: headers}
+
+    context 'when the record exists' do
+      it 'returns the bet' do
+        expect(json).not_to be_empty
+        expect(json['id']).to eq(bet_id)
+      end
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context 'when the record does not exist' do
+      let(:bet_id) { 100 }
+
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404)
+      end
+
+      it 'returns not found message' do
+        expect(response.body).to match(/Couldn't find Bet/)
+      end
+    end
+  end
+
   # test suite for POST /api/v1/bets
   describe 'POST /api/v1/bets' do
 
@@ -30,19 +57,19 @@ RSpec.describe 'Bets API v1', type: :request do
       description: Faker::Lorem.sentence,
       betting_deadline: Time.now + 1.day,
       outcome_deadline: Time.now + 2.day,
-      users: [523, 524, 525],
+      users: [users.map { |user| user.id }],
       possibilities: [
         Faker::Commerce.product_name,
         Faker::Commerce.product_name
       ]
-    }.to_json }
+    } }
 
     context 'when the request is valid' do
-      before { post '/api/v1/bets', params: valid_attributes, headers: headers }
+      before { post '/api/v1/bets', params: valid_attributes.to_json, headers: headers }
 
-      # it 'creates a bet' do
-      #   expect(json['title']).to eq(valid_attributes.title)
-      # end
+      it 'creates a bet' do
+        expect(json['details']['title']).to eq(valid_attributes[:title])
+      end
 
       it 'returns the status code 201' do
         expect(response).to have_http_status(201)

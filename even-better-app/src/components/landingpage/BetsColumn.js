4180504c229dc5c9aec504225c491d50854e6973
Rahul Ramesh <rahul.ramesh888@gmail.com>
2017-10-23 16:11:19 -0400

    Added data to to the bets column

--- even-better-app/src/components/landingpage/BetsColumn.js
+++ even-better-app/src/components/landingpage/BetsColumn.js
@@ -17,36 +17,46 @@ export default class BetsColumn extends Component {
             <th>Result</th>
           </tr>
         </thead>
-        <tbody>
-          <tr>
-            <td>Default</td>
-            <td>Defaultson</td>
-            <td>def@somemail.com</td>
-          </tr>      
+        <tbody>     
           <tr class="success">
-            <td>Success</td>
-            <td>Doe</td>
-            <td>john@example.com</td>
+            <td>SportBet</td>
+            <td>Cavs beat the warriors</td>
+            <td>WIN</td>
           </tr>
           <tr class="danger">
-            <td>Danger</td>
-            <td>Moe</td>
-            <td>mary@example.com</td>
+            <td>Personal Bet</td>
+            <td>Jack shows up late to class</td>
+            <td>LOSE</td>
           </tr>
-          <tr class="info">
-            <td>Info</td>
-            <td>Dooley</td>
-            <td>july@example.com</td>
+          <tr class="success">
+          <td>SportBet</td>
+          <td>Cavs beat the warriors</td>
+          <td>WIN</td>
+        </tr>
+        <tr class="danger">
+          <td>Personal Bet</td>
+          <td>Jack shows up late to class</td>
+          <td>LOSE</td>
+        </tr>
+        <tr class="success">
+            <td>SportBet</td>
+            <td>Cavs beat the warriors</td>
+            <td>WIN</td>
           </tr>
-          <tr class="warning">
-            <td>Warning</td>
-            <td>Refs</td>
-            <td>bo@example.com</td>
+          <tr class="danger">
+            <td>Personal Bet</td>
+            <td>Jack shows up late to class</td>
+            <td>LOSE</td>
           </tr>
-          <tr class="active">
-            <td>Active</td>
-            <td>Activeson</td>
-            <td>act@example.com</td>
+          <tr class="success">
+            <td>SportBet</td>
+            <td>Cavs beat the warriors</td>
+            <td>WIN</td>
+          </tr>
+          <tr class="danger">
+            <td>Personal Bet</td>
+            <td>Jack shows up late to class</td>
+            <td>LOSE</td>
           </tr>
         </tbody>
       </table>

ec92a6cb1379288e718f621e1eef5af681dcc0c7
audishos <audisho.sada@gmail.com>
2017-10-24 16:30:37 -0400

    Moves chatbar logic into ChatBar component

--- even-better-app/src/components/Bet/Bet.jsx
+++ even-better-app/src/components/Bet/Bet.jsx
@@ -3,11 +3,12 @@ import React, { Component } from 'react'
 // Material UI
 import injectTapEventPlugin from 'react-tap-event-plugin'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-import AppBar from 'material-ui/AppBar'
-import FlatButton from 'material-ui/FlatButton';
 
 // Websockets
-import Cable from 'actioncable';
+import Cable from 'actioncable'
+
+// Child components
+import ChatBar from './ChatBar'
 
 class Bet extends Component {
   constructor(props) {
@@ -24,36 +25,32 @@ class Bet extends Component {
 
   render() {
     return(
-      <div className='stage'>
-        <h1>Chat</h1>
-        <div className='chat-logs'>
-          <ul className='chat-logs'>
-            { this.renderChatLog() }
-          </ul>
+      <MuiThemeProvider>
+        <div className='stage'>
+          <h1>Chat</h1>
+          <div className='chat-logs'>
+            <ul className='chat-logs'>
+              { this.renderChatLog() }
+            </ul>
+          </div>
         </div>
-        <input
-          onKeyPress={ (e) => this.handleChatInputKeyPress(e) }
-          value={ this.state.currentChatMessage }
-          onChange={ (e) => this.updateCurrentChatMessage(e) }
-          type='text'
-          placeholder='Enter your message...'
-          className='chat-input' />
-        <button
-          onClick={ (e) => this.handleSendEvent(e) }
-          className='send'>
-          Send
-        </button>
-      </div>
+        <ChatBar
+          currentChatMessage={ this.state.currentChatMessage }
+          updateCurrentChatMessage={ this.updateCurrentChatMessage }
+          handleChatInputKeyPress={ this.handleChatInputKeyPress }
+          handleSendEvent={ this.handleSendEvent }
+        />
+      </MuiThemeProvider>
     )
   }
 
-  updateCurrentChatMessage(event) {
+  updateCurrentChatMessage = (event) => {
     this.setState({
       currentChatMessage: event.target.value
     });
   }
 
-  createSocket() {
+  createSocket = () => {
     let cable = Cable.createConsumer('ws://localhost:3001/cable');
     this.chats = cable.subscriptions.create({
       channel: 'ChatChannel'
@@ -72,7 +69,7 @@ class Bet extends Component {
     });
   }
 
-  handleSendEvent(event) {
+  handleSendEvent = (event) => {
     event.preventDefault();
     this.chats.create(this.state.currentChatMessage);
     this.setState({
@@ -80,13 +77,13 @@ class Bet extends Component {
     });
   }
 
-  handleChatInputKeyPress(event) {
+  handleChatInputKeyPress = (event) => {
     if(event.key === 'Enter') {
       this.handleSendEvent(event);
     }//end if
   }
 
-  renderChatLog() {
+  renderChatLog = () => {
     return this.state.chatLogs.map((el) => {
       return (
         <li key={`chat_${el.id}`}>

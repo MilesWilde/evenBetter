da3551a183db125345afa01f1d7db5cb94d92fc5
Craig Miles-Wilde <craigmilde_@hotmail.com>
2017-10-25 18:48:23 -0400

    fixed merge conflicts - all good

--- even-better-app/src/App.js
+++ even-better-app/src/App.js
@@ -30,34 +30,62 @@ class App extends Component {
     window.scrollTo(0, 0)
   }
   render() {
-    return (
-
-      <MuiThemeProvider >
-        <AppBar
-          style={{position:'fixed'}}
-          title='EvenBetter'
-          iconClassNameRight='muidocs-icon-navigation-expand-more'
-          iconElementRight={
-            <div>
-              <FlatButton label="Sign In" />
-              <FlatButton label="Register" />
-            </div>
-          }
-        />
-        <div style={{ paddingTop: 64 }}></div>
-        <main>
-          <Switch>
-            <Route exact path='/' component={SplashPage} />
-            <Route path='/landing' component={LandingPage}/>
-            <Route path='/signup' component={UserRegistration}/>
-            <Route path='/bets' component={Bet}/>
-            <Route path='/auth/login' component={Login}/>
-          </Switch>
-        </main>
-      </MuiThemeProvider>
-    );
+    if (window.localStorage.auth_token){
+      return (
+        <MuiThemeProvider >
+          <AppBar
+            style={{position:'fixed'}}
+            title={<Link to='/landing'>EvenBetter</Link>}
+            iconClassNameRight='muidocs-icon-navigation-expand-more'
+            iconElementRight={
+              <div>
+                <Link to='/landing'><FlatButton label="Home" /></Link>
+                <Link to='/'><FlatButton label="About" /></Link>
+              </div>
+            }
+          />
+          <div style={{ paddingTop: 64 }}></div>
+          <main>
+            <Switch>
+              <Route exact path='/' component={SplashPage} />
+              <Route path='/landing' component={LandingPage}/>
+              <Route path='/signup' component={UserRegistration}/>
+              <Route path='/bets' component={Bet}/>
+              <Route path='/auth/login' component={Login}/>
+            </Switch>
+          </main>
+        </MuiThemeProvider>
+      )
+    }    
+    else {
+      return (
+        <MuiThemeProvider >
+          <AppBar
+            style={{position:'fixed'}}
+            title={<Link to='/'>EvenBetter</Link>}
+            iconClassNameRight='muidocs-icon-navigation-expand-more'
+            iconElementRight={
+              <div>
+                <Link to='/auth/login'><FlatButton label="Sign In" /></Link>
+                <Link to='/signup'><FlatButton label="Register" /></Link>
+                <Link to='/'><FlatButton label="About" /></Link>
+              </div>
+            }
+          />
+          <div style={{ paddingTop: 64 }}></div>
+          <main>
+            <Switch>
+              <Route exact path='/' component={SplashPage} />
+              <Route path='/landing' component={LandingPage}/>
+              <Route path='/signup' component={UserRegistration}/>
+              <Route path='/bets' component={Bet}/>
+              <Route path='/auth/login' component={Login}/>
+            </Switch>
+          </main>
+        </MuiThemeProvider>
+      )
+    }     
   }
-
 }
 
 export default App

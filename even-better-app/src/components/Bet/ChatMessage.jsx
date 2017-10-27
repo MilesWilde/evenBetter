74be928c2b3710ba0f5add1ed29ece6086423d0f
audishos <audisho.sada@gmail.com>
2017-10-27 00:19:24 -0400

    Adds username and timestamp to chat message with better styling

--- even-better-app/src/components/Bet/ChatMessage.jsx
+++ even-better-app/src/components/Bet/ChatMessage.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Moment from 'moment'
 
 const flexBox = {
   display: 'flex',
@@ -7,12 +8,28 @@ const flexBox = {
   minHeight: '50px'
 }
 
+const styles = {
+  wrapper: {
+    padding: '10px'
+  },
+  user: {
+    fontWeight: 'bold'
+  },
+  content: {
+
+  },
+  timestamp: {
+    fontStyle: 'italic'
+  }
+}
+
 const ChatMessage = (props) => {
 
   return(
-    <div style={ flexBox }>
-      <span>{ props.message.created }: </span>
+    <div style={ styles.wrapper }>
+      <span style={ styles.user }>{ props.message.user } </span>
       <span>{ props.message.content }</span>
+      <div style={ styles.timestamp }>{ Moment(props.message.created_at).local().format('MMM-DD h:mm A') } </div>
     </div>
   )
 

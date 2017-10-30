8fb3f807cef5b00299cc7924e2602ab82691ef7b
audishos <audisho.sada@gmail.com>
2017-10-30 14:28:10 -0400

    Moves stat_scraper into a module sports_helper

--- even-better-server/app/controllers/api/v1/games_controller.rb
+++ even-better-server/app/controllers/api/v1/games_controller.rb
@@ -12,76 +12,6 @@ module Api::V1
   class GamesController < ApplicationController
     def index
 
-      def statScraper(gameVersion, gameDate)
-        urlGameVersion = gameVersion
-        urlGameDate = gameDate
-        html = open("http://scores.nbcsports.msnbc.com/ticker/data/gamesMSNBC.js.asp?sport=#{urlGameVersion}&period=#{urlGameDate}").read
-        sendToReactApp = {
-          gameVersion: urlGameVersion,
-          gameDate: urlGameDate,
-          games: []
-        }
-
-        parsedVar = JSON.parse(html)
-        gameDate = parsedVar['period']
-        gameType = parsedVar['sport']
-        # puts gameDate
-
-        parsedVar['games'].each do |indivGame|
-            # puts indivGame
-
-          indivGame.gsub!('\"', '"')
-          doc = Nokogiri::XML(indivGame)
-        #   puts doc
-
-          #Parsing the escaped XML
-          homeTeamVar = doc.at_xpath('//home-team')
-          awayTeamVar = doc.at_xpath('//visiting-team')
-          gameVariables = doc.at_xpath('//gamestate')
-          tickerVariables = doc.at_xpath('//ticker-entry')
-
-          # All Game Details
-          deliverToClient = {
-              gameType: gameType,
-              gameDate: gameDate,
-              gameTime: gameVariables.attr('gametime'),
-              gameCode: tickerVariables.attr('gamecode'),
-
-              homeTeamName: homeTeamVar.attr('display_name'),
-              homeNickName: homeTeamVar.attr('nickname'),
-              homeTeamLogo: homeTeamVar.at_xpath('//team-logo').attr('link'),
-              homeTeamImage: homeTeamVar.at_xpath('//team-logo').attr('gz-image'),
-              homeScore: homeTeamVar.attr('score'),
-              homeAlias: homeTeamVar.attr('alias'),
-
-              awayTeamName:awayTeamVar.attr('display_name'),
-              awayNickName:awayTeamVar.attr('nickname'),
-              awayTeamLogo: awayTeamVar.at_xpath('//team-logo').attr('link'),
-              awayTeamImage: awayTeamVar.at_xpath('//team-logo').attr('gz-image'),
-              awayScore:awayTeamVar.attr('score'),
-              awayAlias:awayTeamVar.attr('alias')
-          }
-
-
-          # Deciding Winner and Loser
-          if deliverToClient[:homeScore].to_i > deliverToClient[:awayScore].to_i
-              deliverToClient[:gameWinner] = deliverToClient[:homeTeamName] + " " + deliverToClient[:homeNickName]
-          elsif deliverToClient[:homeScore] === ''
-              deliverToClient[:gameWinner] = nil
-          elsif deliverToClient[:homeScore].to_i < deliverToClient[:awayScore].to_i
-              deliverToClient[:gameWinner] = deliverToClient[:awayTeamName] + " " + deliverToClient[:awayNickName]
-          else 
-              deliverToClient[:gameWinner] = "Tie game"
-          end
-
-          #JSON VERSION
-          sendToReactApp[:games].push(deliverToClient)
-        end
-        sendToReactAppJson = sendToReactApp.to_json
-        return sendToReactAppJson
-      end
-
-
       # params contains the sporttype and gameDate
       puts params.inspect
       puts params["sport"]
@@ -90,10 +20,9 @@ module Api::V1
 
       dateForApi = params["gameDate"][0,10].gsub! '-',''
 
-      @scores = statScraper(params["sport"], dateForApi)
+      @scores = SportsHelper::stat_scraper(params["sport"], dateForApi)
       render json: @scores
     end
   end
-
 
 end
---
+++ even-better-server/app/lib/sports_helper.rb
@@ -0,0 +1,69 @@
+module SportsHelper
+  def self.stat_scraper(gameVersion, gameDate)
+    urlGameVersion = gameVersion
+    urlGameDate = gameDate
+    html = open("http://scores.nbcsports.msnbc.com/ticker/data/gamesMSNBC.js.asp?sport=#{urlGameVersion}&period=#{urlGameDate}").read
+    sendToReactApp = {
+      gameVersion: urlGameVersion,
+      gameDate: urlGameDate,
+      games: []
+    }
+
+    parsedVar = JSON.parse(html)
+    gameDate = parsedVar['period']
+    gameType = parsedVar['sport']
+    # puts gameDate
+
+    parsedVar['games'].each do |indivGame|
+        # puts indivGame
+
+      indivGame.gsub!('\"', '"')
+      doc = Nokogiri::XML(indivGame)
+    #   puts doc
+
+      #Parsing the escaped XML
+      homeTeamVar = doc.at_xpath('//home-team')
+      awayTeamVar = doc.at_xpath('//visiting-team')
+      gameVariables = doc.at_xpath('//gamestate')
+      tickerVariables = doc.at_xpath('//ticker-entry')
+
+      # All Game Details
+      deliverToClient = {
+          gameType: gameType,
+          gameDate: gameDate,
+          gameTime: gameVariables.attr('gametime'),
+          gameCode: tickerVariables.attr('gamecode'),
+
+          homeTeamName: homeTeamVar.attr('display_name'),
+          homeNickName: homeTeamVar.attr('nickname'),
+          homeTeamLogo: homeTeamVar.at_xpath('//team-logo').attr('link'),
+          homeTeamImage: homeTeamVar.at_xpath('//team-logo').attr('gz-image'),
+          homeScore: homeTeamVar.attr('score'),
+          homeAlias: homeTeamVar.attr('alias'),
+
+          awayTeamName:awayTeamVar.attr('display_name'),
+          awayNickName:awayTeamVar.attr('nickname'),
+          awayTeamLogo: awayTeamVar.at_xpath('//team-logo').attr('link'),
+          awayTeamImage: awayTeamVar.at_xpath('//team-logo').attr('gz-image'),
+          awayScore:awayTeamVar.attr('score'),
+          awayAlias:awayTeamVar.attr('alias')
+      }
+
+
+      # Deciding Winner and Loser
+      if deliverToClient[:homeScore].to_i > deliverToClient[:awayScore].to_i
+          deliverToClient[:gameWinner] = deliverToClient[:homeTeamName] + " " + deliverToClient[:homeNickName]
+      elsif deliverToClient[:homeScore] === ''
+          deliverToClient[:gameWinner] = nil
+      elsif deliverToClient[:homeScore].to_i < deliverToClient[:awayScore].to_i
+          deliverToClient[:gameWinner] = deliverToClient[:awayTeamName] + " " + deliverToClient[:awayNickName]
+      else
+          deliverToClient[:gameWinner] = "Tie game"
+      end
+
+      #JSON VERSION
+      sendToReactApp[:games].push(deliverToClient)
+    end
+    return sendToReactApp.to_json
+  end
+end

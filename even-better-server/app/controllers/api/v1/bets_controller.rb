00527a08e1907927985d03374e81d78833561012
Craig Miles-Wilde <craigmilde_@hotmail.com>
2017-10-30 11:10:15 -0400

    added migration for gametype gamedate gamecode

--- even-better-server/app/controllers/api/v1/bets_controller.rb
+++ even-better-server/app/controllers/api/v1/bets_controller.rb
@@ -53,8 +53,8 @@ module Api::V1
 
       finished_games = []
       # find current user's bets
+      # check api/v1/games for gamecodes of unfinished_game_array
       # unfinished_game_array = Bet.where(outcome_id: nil).where(user_id: current_user.id).where(gamecode: !nil)
-      # # check api/v1/games for gamecodes of unfinished_game_array
       # # something like:
       # # loop through all games without possibility chosen
       # unfinished_game_array.each do |unfinished_game|
